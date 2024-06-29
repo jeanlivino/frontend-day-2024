438cc9022281d4ee15d5348f1a0a5fed02d86485
matheus-franca <matheussfrancads2345@gmail.com>
2024-06-29 19:16:42 -0300

    create a infos section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
+import { InfosSection } from '@/components/InfosSection';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
@@ -8,6 +9,7 @@ export default function Home() {
     <main>
       <Header />
       <HeroSection />
+      <InfosSection />
     </main>
   );
 }

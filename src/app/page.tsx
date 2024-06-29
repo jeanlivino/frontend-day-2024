fe81694400124c47c01832d384c74eca4c31e076
Jean Livino <jeanlivino@gmail.com>
2024-06-29 17:31:01 -0300

    add fonts to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button>Click me</Button>
+      <Button className='font-kdam'>Click me</Button>
     </main>
   );
 }

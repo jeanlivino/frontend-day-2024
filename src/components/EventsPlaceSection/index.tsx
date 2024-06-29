d6ac43dcc055b2663420e69162eb361f321889db
matheus-franca <matheussfrancads2345@gmail.com>
2024-06-29 20:34:30 -0300

    feat: create events place

--- src/components/EventsPlaceSection/index.tsx
+++ src/components/EventsPlaceSection/index.tsx
@@ -1,33 +1,35 @@
 import Image from "next/image";
+import { Button } from "../ui/button";
 
 function EventsPlaceSection() {
   return (
-    <section className="container mt-28">
-      <div className="container">
+    <section className="container mt-16">
+      <div className="flex items-center gap-36 w-full max-lg:flex-col">
         <Image
-          className="absolute w-80 z-0 rounded-3xl"
-          src="/front-part.png"
+          src="/event-place.png"
           alt="Fábrica de Negócios"
-          width={567}
-          height={474}
+          width={550}
+          height={400}
         />
-        <Image
-          className="absolute w-52 z-20 rounded-3xl"
-          src="/room.png"
-          alt="Sala"
-          width={434}
-          height={268}
-        />
-        <Image
-          className="relative z-10 rounded-3xl left-60"
-          src="/fabrica-negocios.png"
-          alt="Fábrica de Negócios"
-          width={119}
-          height={143}
-        />
-        <h2>Local do Evento</h2>
-        <h3>Fábrica de Negócios</h3>
-        <p>Av. Monsenhor Tabosa, 740 Centro – Fortaleza – CE</p>
+        <div className="flex flex-col gap-2">
+          <h2 className="font-kdam uppercase text-5xl text-purple-950 mb-9">
+            Local do Evento
+          </h2>
+          <h3 className="font-bold text-pink-600 text-3xl">
+            Fábrica de Negócios
+          </h3>
+          <div className="flex gap-3 flex-col justify-between">
+            <p className="text-lg">
+              Av. Monsenhor Tabosa, 740 <br /> Centro – Fortaleza – CE
+            </p>
+            <Button
+              className="border border-purple-950 bg-white text-purple-950 font-semibold text-base max-w-48 hover:bg-purple-600 hover:text-slate-50 hover:border-l-purple-700"
+              size={"lg"}
+            >
+              Como chegar
+            </Button>
+          </div>
+        </div>
       </div>
     </section>
   );
